f80e36eb37b1573196ae48c3850d35637c150cc5
ChungHokage <chunglight01@gmail.com>
2024-06-08 15:03:03 +0700

    add Image table

---
+++ X.Data/Entities/Image.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace X.Data.Entities
+{
+    public class Image
+    {
+        public Guid Id { get; set; }
+        public string Path { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public double Size { get; set; }
+        public DateTime UploadTime { get; set; }
+    }
+}
