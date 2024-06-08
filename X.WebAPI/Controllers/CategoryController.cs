12886c4b147a3f1f1ca0f97d9c6d9454937410be
ChungHokage <chunglight01@gmail.com>
2024-06-09 00:24:19 +0700

    write some api for product and category

---
+++ X.WebAPI/Controllers/CategoryController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http.HttpResults;
+using X.Application.Request.Category;
+using X.WebAPI.Services.Interfaces;
+using Azure.Core;
+using Org.BouncyCastle.Utilities;
+
+namespace X.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpPost("add")]
+        public async Task<IActionResult> Add([FromBody] CategoryRequest request)
+        {
+            var result = await _categoryService.Create(request);
+            if (result == null)
+            {
+                return BadRequest(result);
+            }
+            else
+            {
+                return Ok(result);
+            }
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var result = await _categoryService.Delete(id);
+            if (result.isSuccessed)
+            {
+                return Ok(result);
+            }
+            else { return BadRequest(result); }
+        }
+
+        [HttpGet("get")]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = await _categoryService.GetAll();
+            if (result.isSuccessed)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return BadRequest(result.Message);
+            }
+        }
+
+        [HttpGet("get/{id}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            var result = await _categoryService.GetById(id);
+            if (result.isSuccessed)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return BadRequest(result.Message);
+            }
+        }
+    }
+}
---
+++ X.WebAPI/Controllers/ProductController.cs
@@ -0,0 +1,44 @@
+﻿using Azure.Core;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using X.Application.Request.Product;
+using X.WebAPI.Services.Interfaces;
+
+namespace X.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpPost("add-product")]
+        public async Task<IActionResult> Add([FromBody] ProductRequest request)
+        {
+            var result = await _productService.CreateProduct(request);
+            if (result.isSuccessed) { return Ok(result); }
+            else { return BadRequest(result.Message); }
+        }
+
+        [HttpPost("add-color")]
+        public async Task<IActionResult> AddColor([FromBody] ColorRequest request)
+        {
+            var result = await _productService.CreateColor(request);
+            if (result.isSuccessed) { return Ok(result); }
+            else { return BadRequest(result.Message); }
+        }
+
+        [HttpPost("add-details")]
+        public async Task<IActionResult> AddDetails(Guid productId, ProductDetailRequest request)
+        {
+            var result = await _productService.CreateProductDetails(productId, request);
+            if (result.isSuccessed) { return Ok(result); }
+            else { return BadRequest(result.Message); }
+        }
+    }
+}
